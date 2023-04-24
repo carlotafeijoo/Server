35147460703085ba37ff3999833d23d0e6d3ec44
mariagomezpalla <maria.gomezpalla@usp.ceu.es>
2023-04-24 18:05:28 +0200

    fix errors

--- src/Interfaces/ElderlyManager.java
+++ src/Interfaces/ElderlyManager.java
@@ -1,5 +1,22 @@
 package Interfaces;
+import java.util.List;
+import POJOS.Elderly;
+import POJOS.Staff;
 
-public class ElderlyManager {
+public interface ElderlyManager {
+	    //Adding a new elderly
+		public void deleteElderly(Elderly e);
+		
+		//get the list of the elderlies
+		public List<Elderly> getListOfElderlies();
+		//Delete an elderly
+		public void addElderly(Elderly e);
+		
+		//Assign a dog to a staff member to a elderly
+		public void assign(int staff_ID, int elderly_ID);
+		
+		//Update info from elderly
+		public void updateInfo(Elderly e);
+		
 
 }
--- src/jdbc/JDBCElderlyManager.java
+++ src/jdbc/JDBCElderlyManager.java
@@ -2,15 +2,40 @@ package jdbc;
 
 import POJOS.Elderly;
 
-public interface JDBCElderlyManager {
-	JDBCManager manager;
-	
-	public JDBCElderlytManager(JDBCManager m) {
-		this.manager = m;
+import java.util.List;
+
+import Interfaces.ElderlyManager;
+
+public class JDBCElderlyManager implements ElderlyManager{
+
+	@Override
+	public void addElderly(Elderly d) {
+		// TODO Auto-generated method stub
+		
 	}
-   //add a patient to a database
-	
-	
-	
+
+	@Override
+	public List<Elderly> getListOfElderlies() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void assign(int staff_ID, int elderly_ID) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteElderly(Elderly d) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void updateInfo(Elderly e) {
+		// TODO Auto-generated method stub
+		
+	}
 	
 }
