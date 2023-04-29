16ffee88e0261e0c1d13f73a921cba2a9db3a524
mariagomezpalla <maria.gomezpalla@usp.ceu.es>
2023-04-29 19:52:22 +0200

    jdbc an interface of performsmanager

--- src/jdbc/JDBCPerformsManager.java
+++ src/jdbc/JDBCPerformsManager.java
@@ -1,5 +1,31 @@
 package jdbc;
 
-public class JDBCPerformsManager {
+import Interfaces.PerformsManager;
+import POJOS.FamilyContact;
+import POJOS.Performs;
+import POJOS.Shift;
+import POJOS.Staff;
+import POJOS.Task;
+
+public class JDBCPerformsManager implements PerformsManager {
+private JDBCManager manager;
+	
+	public JDBCPerformsManager(JDBCManager m)
+	{
+		this.manager = m;
+	}
+	
+	//assign a new shift to a staff
+	public void assignShift (Shift shift, Staff staff) {//TODO
+		
+	}
+	//unassign a new shift to a staff
+		public void unassignShift (Shift shift, Staff staff) { //TODO
+			
+		}
+		//show the schedule from a staff
+		public void schedule (Performs performs, Staff staff, Task task) {//TODO
+			
+		}
 
 }
