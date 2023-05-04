c8f68f30d645d7679ff86072a173474bc4c3eed4
carbeluche <118633320+carbeluche@users.noreply.github.com>
2023-05-04 21:15:30 +0200

    Connection funcionando, sentencias crear tablas

--- src/jdbc/JDBCManager.java
+++ src/jdbc/JDBCManager.java
@@ -14,8 +14,8 @@ public class JDBCManager {
 	    try {            
 	        // Open the DB connection
 	        Class.forName("org.sqlite.JDBC");
-	        String dbPath = System.getProperty("user.dir") ;
-	        c = DriverManager.getConnection("jdbc:sqlite:" + dbPath);
+	        String dbPath = "/db/ResidencialArea.db";
+	        c = DriverManager.getConnection("jdbc:sqlite:." +dbPath );
 	        System.out.println("Database connection opened.");
 
 	        //create tables
@@ -34,41 +34,41 @@ public class JDBCManager {
 		// Create Tables
 		try {
 		Statement stmt = c.createStatement();
-		String sql = "CREATE TABLE Elderly ("
+		String sql = "CREATE TABLE elderly ("
 		+ "	elderly_id	    INTEGER PRIMARY KEY AUTOINCREMENT,"
 		+ "	name			TEXT NOT NULL,"
-		+ "	age				INTEGER NOT NULL,"
+		+ "	age				INTEGER NOT NULL"
 		+ ");";
 		stmt.executeUpdate(sql);
-		sql = "CREATE TABLE Family_contact ("
+		sql = "CREATE TABLE family_contact ("
 		+ "	family_id	INTEGER PRIMARY KEY AUTOINCREMENT,"
-		+ "	elderly_id	INTEGER FOREIGN KEY AUTOINCREMENT,"
+		+ "	elderly_id	INTEGER,"
 		+ "	name		TEXT NOT NULL,"
 		+ "	adress		TEXT NOT NULL,"
 		+ "	phone		INTEGER NOT NULL,"
 		+ "	email		TEXT NOT NULL,"
-		+ "	elderly_id	INTEGER NOT NULL REFERENCES Elderly(elderly_id) ON DELETE RESTRICT"
+		+ "	FOREIGN KEY(elderly_id) REFERENCES Elderly(id) ON DELETE RESTRICT,"
 		+ ");";
 		stmt.executeUpdate(sql);
-		sql = "CREATE TABLE Staff ("
+		sql = "CREATE TABLE staff ("
 		+ "	staff_id	INTEGER PRIMARY KEY AUTOINCREMENT,"
 		+ "	name		TEXT NOT NULL,"
 		+ "	DOB 		DATE,"
 		+ "	adress		TEXT NOT NULL,"
-		+ "	phone		INTEGER NOT NULL,"
+		+ "	phone		INTEGER NOT NULL"
 		+ ");";
 		stmt.executeUpdate(sql);
-		sql = "CREATE TABLE Tasks ("
+		sql = "CREATE TABLE tasks ("
 		+ "	task_id			INTEGER PRIMARY KEY AUTOINCREMENT,"
-		+ "	description		TEXT NOT NULL,"
+		+ "	description		TEXT NOT NULL"
 		+ ");";
 		stmt.executeUpdate(sql);
-		sql = "CREATE TABLE Performs ("
+		sql = "CREATE TABLE performs ("
 		+ "	performs_id		INTEGER PRIMARY KEY AUTOINCREMENT,"
 		+ "	task_id			INTEGER AUTOINCREMENT,"
 		+ "	staff_id	 	INTEGER AUTOINCREMENT,"
-		+ "	staff_id		INTEGER NOT NULL REFERENCES Staff(staff_id) ON DELETE RESTRICT\""
-		+ " task_id			INTEGER NOT NULL REFERENCES Tasks(task_id) ON DELETE RESTRICT\""
+		+ "	staff_id		INTEGER NOT NULL REFERENCES Staff(staff_id) ON DELETE RESTRICT,"
+		+ " task_id			INTEGER NOT NULL REFERENCES Tasks(task_id) ON DELETE RESTRICT"
 		+ ");";
 		stmt.executeUpdate(sql);
 		} catch (SQLException e) {
