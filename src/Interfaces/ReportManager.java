3ce6933e3c34ca394b8cbfbc36a5c9a50100f76e
natalia <natalia@localhost>
2023-11-16 11:45:42 +0100

    report function

--- src/Interfaces/ReportManager.java
+++ src/Interfaces/ReportManager.java
@@ -1,6 +1,12 @@
 package Interfaces;
 
+import java.sql.SQLException;
+
+import POJOS.Report;
+
 public interface ReportManager {
+
+	Report seeBitalinoReportByID(int report_id) throws SQLException;
 
 	
 }
--- src/jdbc/JDBCReportManager.java
+++ src/jdbc/JDBCReportManager.java
@@ -2,11 +2,12 @@ package jdbc;
 
 import java.sql.Date;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import Interfaces.ReportManager;
-import POJOS.Bitalino;
-//crear proyecto Bitalino 
+import POJOS.Report;
+
 
 public class JDBCReportManager implements ReportManager {
 	
@@ -16,8 +17,37 @@ public class JDBCReportManager implements ReportManager {
 		this.ReportManager = jdbcManager;
 	}
 	
+	@Override
+	public Report seeBitalinoReportByID(int report_id) throws SQLException {
+		
+		Report report = null;
+		
+		try {
+			String sql = "SELECT * FROM Report WHERE id = ?";
+			PreparedStatement prep = ReportManager.getConnection().prepareStatement(sql);
+			prep.setInt(1, report_id);
+			ResultSet resultSet = prep.executeQuery();
+			
+			while(resultSet.next()) {
+				String file_name = resultSet.getString("file_name");
+				Integer task_id = resultSet.getInt("task_id");
+				Integer elderly_id = resultSet.getInt("elderly_id");
+				
+				report = new Report(report_id, file_name, task_id, elderly_id);
+		
+			prep.close();
+			}	
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return report;
+	}
 	
 	
 	
 
 }
+
+
